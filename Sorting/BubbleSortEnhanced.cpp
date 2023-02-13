c1f7f41429c01c4781cfb8eb25dc4b86b188e734
Shreyash-kant <priyankkant555@gmail.com>
2023-02-13 16:55:41 +0530

    bubble sort with flag

--- Sorting/BubbleSortEnhanced.cpp
+++ Sorting/BubbleSortEnhanced.cpp
@@ -7,17 +7,23 @@ void swap(int *p,int *q){
 }
 void BubbleSort(int arr[], int n){
 	for(int i=0;i<n-1;++i){
+		bool swapElements=false;
 		for(int j=0;j<n-1-i;++j){
-			if(arr[j]>arr[j+1])
+			if(arr[j]>arr[j+1]){
 			swap((arr+j),(arr+j+1));
+			swapElements=true;	
+			}
+			
 		}
+		if(swapElements==false)
+		break;
 	}
 	cout << "sorted array:";
 	for(int i=0;i<n;++i)
 	cout << arr[i] << " ";
 }
 int main(){
-	int arr[10] = {1,34,56,3,5,9,2,7,6,8};
+	int arr[10] = {1,2,3,3,5,9,7,6,8,67};
 	BubbleSort(arr,10);
 	return 0;
 }
